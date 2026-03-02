defb4aec4675ddc98f8b19f06edf2a9fa3d19928
Nandan Navale <nandan.navale@gmail.com>
2026-03-02 10:56:58 +0530

    first commit

---
+++ FinanceApp/FinanceApp/Data/Service/IExpensesService.cs
@@ -0,0 +1,16 @@
+﻿using FinanceApp.Models;
+
+namespace FinanceApp.Data.Service
+{
+    public interface IExpensesService
+    {
+        Task<IEnumerable<Expense>> GetAll();
+
+        Task Add(Expense expense);
+        //Task Update(Expense expense);
+        //Task Delete(Expense expense);
+        //Task<Expense> GetById(int id);
+        public IQueryable GetChartData();
+        
+    }
+}
---
+++ FinanceApp/FinanceApp/Models/Expense.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage ="Amount needs to be")]
+        public double Amount { get; set; }
+        [Required]
+        public string Category {  get; set; }=null!;
+
+        public DateTime date { get; set; } = DateTime.Now;
+
+    }
+}
